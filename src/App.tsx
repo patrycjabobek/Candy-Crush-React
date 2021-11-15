fceb0fa6d423deaca362eafa5a82011ca94d47b0
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-15 12:18:46 +0100

    Added function CreateBoard and made it display

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const width = 8;
 const candyColours = [
@@ -13,8 +13,35 @@ const candyColours = [
 
 
 function App() {
+
+    const [currentColorArangement, setCurrentColorArrangement] = useState<string[]>([] )
+
+    function createBoard() {
+        const randomColorArrangement = [];
+
+        for (let i = 0; i < width * width; i++){
+            const randomNumberFrom0To5 = Math.floor(Math.random() * candyColours.length);
+            const randomColor = candyColours[randomNumberFrom0To5];
+
+            randomColorArrangement.push(randomColor);
+        }
+        setCurrentColorArrangement(randomColorArrangement);
+    }
+
+    useEffect(() => {
+        createBoard();
+    }, [width]); // if the width would change the createBoard will rerender
+
   return (
-    <div> Hi!
+    <div className="app">
+        <div className="game">
+            {currentColorArangement.map((candyColor: string, index: number) => (
+                <img
+                    key={index}
+                    style={{backgroundColor: candyColor}}
+                />)
+            )}
+        </div>
     </div>
   );
 }
