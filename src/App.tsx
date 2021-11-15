e34ddfe654457ddcc03f325e4a54c22f74b4d5ac
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-15 12:39:34 +0100

    Added logic for checking for the same color in columns of four and three

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,30 @@ const candyColours = [
 
 function App() {
 
-    const [currentColorArangement, setCurrentColorArrangement] = useState<string[]>([] )
+    const [currentColorArrangement, setCurrentColorArrangement] = useState<string[]>([] )
+
+    function checkForColumnOfFour() {
+        for (let i = 0; i < 39; i++) {
+            const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+            const decidedColor = currentColorArrangement[i];
+
+            if (columnOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                columnOfFour.forEach(square => currentColorArrangement[square] = '');
+            }
+        }
+    }
+
+    function checkForColumnOfThree() {
+        for (let i = 0; i < 47; i++) {
+            const columnOfThree = [i, i + width, i + width * 2];
+            const decidedColor = currentColorArrangement[i];
+
+            if (columnOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                columnOfThree.forEach(square => currentColorArrangement[square] = '');
+            }
+        }
+    }
+
 
     function createBoard() {
         const randomColorArrangement = [];
@@ -30,15 +53,26 @@ function App() {
 
     useEffect(() => {
         createBoard();
-    }, [width]); // if the width would change the createBoard will rerender
+    }, []); // if there was width: if the width would change the createBoard will rerender
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            checkForColumnOfFour();
+            checkForColumnOfThree();
+            setCurrentColorArrangement([...currentColorArrangement])
+        }, 100)
+        return () => clearInterval(timer);
+    }, [checkForColumnOfThree(), checkForColumnOfFour(), currentColorArrangement])
+
 
   return (
     <div className="app">
         <div className="game">
-            {currentColorArangement.map((candyColor: string, index: number) => (
+            {currentColorArrangement.map((candyColor: string, index: number) => (
                 <img
                     key={index}
                     style={{backgroundColor: candyColor}}
+                    alt={candyColor}
                 />)
             )}
         </div>
