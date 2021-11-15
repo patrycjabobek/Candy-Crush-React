d956b68b23d82db836da130c5ddecadf7f70a68f
Tarzanica <patrycja.bobek@microsoft.wsei.edu.pl>
2021-11-15 12:51:15 +0100

    Added logic for finding squares of the same color in rows of four and three

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,31 @@ function App() {
         }
     }
 
+    function checkForRowOfFour() {
+        for (let i = 0; i < 64; i++) {
+            const rowOfFour = [i, i + 1, i + 2, i + 3];
+            const decidedColor = currentColorArrangement[i];
+            const notValid = [5,6,7,13,14,15,21,22,23,29,30,31,37,38,39,45,46,47,53,54,55,62,63,64];
+
+            if (notValid.includes(i)) continue;
+            if (rowOfFour.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfFour.forEach(square => currentColorArrangement[square] = '');
+            }
+        }
+    }
+    function checkForRowOfThree() {
+        for (let i = 0; i < 60; i++) {
+            const rowOfThree = [i, i + 1, i + 2];
+            const decidedColor = currentColorArrangement[i];
+            const notValid = [6,7,14,15,22,23,30,31,38,39,46,47,54,55,63,64];
+
+            if (notValid.includes(i)) continue;
+
+            if (rowOfThree.every(square => currentColorArrangement[square] === decidedColor)) {
+                rowOfThree.forEach(square => currentColorArrangement[square] = '');
+            }
+        }
+    }
 
     function createBoard() {
         const randomColorArrangement = [];
@@ -59,10 +84,12 @@ function App() {
         const timer = setInterval(() => {
             checkForColumnOfFour();
             checkForColumnOfThree();
+            checkForRowOfFour();
+            checkForRowOfThree();
             setCurrentColorArrangement([...currentColorArrangement])
         }, 100)
         return () => clearInterval(timer);
-    }, [checkForColumnOfThree(), checkForColumnOfFour(), currentColorArrangement])
+    }, [checkForColumnOfThree(), checkForColumnOfFour(),checkForRowOfThree(),checkForRowOfFour(),  currentColorArrangement])
 
 
   return (
